80c425442ef7de48557d2108365aa2acea62e396
sripwoud <me@sripwoud.xyz>
2024-10-07 19:10:28 +0200

    ci: define fly deploy workflow (#7)

--- apps/client/src/trpc.ts
+++ apps/client/src/trpc.ts
@@ -1,7 +1,7 @@
 import type { AppRouter } from '@server/trpc/trpc.router'
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client'
 
-const PORT = 3001 // TODO: make this configurable and/or use env vars
+const PORT = process.env.NODE_ENV === 'production' ? 'https://rideau.fly.dev' : 3001 // TODO: make this configurable and/or use env vars
 
 export const trpc = createTRPCProxyClient<AppRouter>({
   links: [
