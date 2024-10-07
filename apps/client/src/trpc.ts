834eb93b14f511fa301d521c541e34a4d821e639
sripwoud <me@sripwoud.xyz>
2024-10-08 00:06:23 +0200

    fix(ui): update url of trpc proxy client

--- apps/client/src/trpc.ts
+++ apps/client/src/trpc.ts
@@ -1,12 +1,15 @@
 import type { AppRouter } from '@server/trpc/trpc.router'
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client'
 
-const PORT = process.env.NODE_ENV === 'production' ? 'https://rideau.fly.dev' : 3001 // TODO: make this configurable and/or use env vars
+// TODO: make this configurable and/or use env vars
+const PORT = 3001
+const PROD_URL = 'https://rideau.fly.dev'
+const url = process.env.NODE_ENV === 'production' ? PROD_URL : `http://localhost:${PORT}`
 
 export const trpc = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: `http://localhost:${PORT}/trpc`,
+      url: `${url}/trpc`,
     }),
   ],
 })
