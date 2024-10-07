1272021104f9b9a1f3a76d9f27bec807c75328c9
sripwoud <me@sripwoud.xyz>
2024-10-08 00:25:01 +0200

    style(client): set metadata (icon, title...)

--- apps/client/src/components/Header.tsx
+++ apps/client/src/components/Header.tsx
@@ -1,10 +1,14 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 export const Header = () => (
   <header className='p-4'>
     <ul className='flex items-center justify-between'>
-      <li className='text-4xl'>
-        <Link href='/'>RIDEAU</Link>
+      <li>
+        <Link className='flex items-center' href='/'>
+          <Image className='' src='/icon.png' alt='rideau-logo' height={50} width={50} />
+          <span className='text-4xl'>RIDEAU</span>
+        </Link>
       </li>
       <div className='flex space-x-4'>
         <li>
