2e176ec93413fb2b7060ca734539c3b82b531285
sripwoud <me@sripwoud.xyz>
2024-10-09 17:19:58 +0200

    chore(client): tsignore

--- apps/client/src/components/LoginForm.tsx
+++ apps/client/src/components/LoginForm.tsx
@@ -21,11 +21,13 @@ export const LoginForm = () => {
     if (email.isNone()) return
     // TODO: use Result instead of try/catch
     try {
+      // @ts-expect-error magic has an ugly union type FIXME
       const didToken = await magic.auth.loginWithMagicLink({ email: email.get() })
       setHeaders({ Authorization: `Bearer ${didToken}` })
       const res = await trpc.auth.login.query()
 
       if ('authenticated' in res && res.authenticated === true) {
+        // @ts-expect-error magic has an ugly union type FIXME
         const userMetadata = await magic.user.getMetadata()
         setUser(Some.create(userMetadata))
         router.push('/dashboard')
