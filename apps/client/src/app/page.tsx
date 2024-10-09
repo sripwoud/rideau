54df9609505f4734f1f94d5e02b20402ad548cc9
sripwoud <me@sripwoud.xyz>
2024-10-09 16:48:32 +0200

    feat(client): persist authentication state

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,15 +1,12 @@
 // import { trpc } from '@client/trpc'
 'use client'
-import { QrCode } from 'client/c/QrCode'
-import { useQRCodeScan } from 'client/h/useScanQrCode'
+// import { QrCode } from 'client/c/QrCode'
+// import { useQRCodeScan } from 'client/h/useScanQrCode'
+import { useLogin } from 'client/h/useLogin'
 
 export default function Home() {
   // const { greeting } = await trpc.hello.query({ name: 'sripwoud rendered server side' })
-  const { isLoading, handleScan, scanned } = useQRCodeScan()
-
-  return (
-    <div>
-      {isLoading ? <p>loading...</p> : <QrCode value='https://www.pse.dev' onScan={handleScan} isScanned={scanned} />}
-    </div>
-  )
+  // const { isLoading, handleScan, scanned } = useQRCodeScan()
+  useLogin()
+  return <p>loading...</p>
 }
