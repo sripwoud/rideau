54f2f812e72ccfe421f6403828f1fed69f0d1dd1
sripwoud <me@sripwoud.xyz>
2024-10-15 16:36:35 +0200

    chore: add gasPolicyId in config

--- apps/client/src/lib/config.ts
+++ apps/client/src/lib/config.ts
@@ -1,14 +1,15 @@
 import { isEnvVarDefined, sharedConfig, type SharedConfigI } from 'config'
 import type { Metadata } from 'next'
 import { arbitrumSepolia } from 'viem/chains'
+import { Address } from 'viem/types'
 
-type HexString = `0x${string}`
-export enum Contract {
+enum Contract {
   Semaphore = 'semaphore',
   YesNoFeedback = 'yesNoFeedback',
 }
 interface ClientConfigI {
-  contracts: Record<Contract, Record<number, HexString>>
+  contracts: Record<Contract, Record<number, Address>>
+  gasPolicyId: string
   metadata: Metadata
   serverUrl: string
 }
@@ -24,9 +25,10 @@ const clientConfig: SharedConfigI & ClientConfigI = {
     description: 'Anonymous survey and feedback platform',
   },
   contracts: {
-    [Contract.YesNoFeedback]: { [arbitrumSepolia.id]: '0x4308bfd6c6cd829b6d342c90b43bef46700cec8d' as HexString },
-    [Contract.Semaphore]: { [arbitrumSepolia.id]: '0x1e0d7FF1610e480fC93BdEC510811ea2Ba6d7c2f' as HexString },
+    [Contract.YesNoFeedback]: { [arbitrumSepolia.id]: '0x4308bfd6c6cd829b6d342c90b43bef46700cec8d' },
+    [Contract.Semaphore]: { [arbitrumSepolia.id]: '0x1e0d7FF1610e480fC93BdEC510811ea2Ba6d7c2f' },
   },
+  gasPolicyId: '82ec857f-1f2a-4f18-9104-f7fbb9b8ead5',
   serverUrl,
 }
 
