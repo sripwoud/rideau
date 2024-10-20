88ebbaff84b9812fdba33fc8608bfa1b52ea298d
sripwoud <me@sripwoud.xyz>
2024-10-20 23:17:04 +0200

    chore: add comment

--- apps/client/src/lib/react-query.ts
+++ apps/client/src/lib/react-query.ts
@@ -1,5 +1,8 @@
 import { QueryClient as _TrpcQueryClient } from '@tanstack/react-query-4'
 import { QueryClient as _AlchemyQueryClient } from '@tanstack/react-query-5'
 
+// alchemy account kit needs @tanstack/react-query@^5
+// @trpc/react-query needs @tanstack/react-query@^4
+// see overrides set in package.json to accomodate using both versions simultaneously
 export const AlchemyQueryClient = () => new _AlchemyQueryClient()
 export const TrpcQueryClient = () => new _TrpcQueryClient()
