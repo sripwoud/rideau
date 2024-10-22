5338898f8b31effe215e006fd41cd023d2892bf5
sripwoud <me@sripwoud.xyz>
2024-10-22 17:15:12 +0200

    chore(server): add logs

--- apps/server/src/bandada/bandada.service.ts
+++ apps/server/src/bandada/bandada.service.ts
@@ -1,5 +1,6 @@
 import { ApiSdk } from '@bandada/api-sdk'
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
+
 import type {
   AddMemberDto,
   CreateGroupDto,
@@ -11,13 +12,18 @@ import { serverConfig } from 'server/l/config'
 
 @Injectable()
 export class BandadaService {
+  logger = new Logger(BandadaService.name)
   sdk = new ApiSdk(serverConfig.bandada.url)
   private apiKey = serverConfig.bandada.apiKey
 
   async addMember({ groupId, memberId }: AddMemberDto) {
     // TODO: handle/bubble error or check first that member already joins, otherwise this returns 400
     // https://github.com/bandada-infra/bandada/blob/5b19cf7dd3e9353316c9ae54a353a766d3cfe1c1/apps/api/src/app/groups/groups.service.ts#L426
-    return this.sdk.addMemberByApiKey(groupId, memberId, this.apiKey)
+    try {
+      return await this.sdk.addMemberByApiKey(groupId, memberId, this.apiKey)
+    } catch (error) {
+      this.logger.error(error)
+    }
   }
 
   async createGroup(createGroupDto: CreateGroupDto) {
