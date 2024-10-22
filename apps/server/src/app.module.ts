2fa2dcb699a3311395e747f81c6523c288ab2b26
sripwoud <me@sripwoud.xyz>
2024-10-22 11:48:43 +0200

    feat(server): inject `commitment` in whoami response (#44)

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common'
 import { AlchemyProxyController } from 'server/alchemy-proxy/alchemy-proxy.controller'
 import { AppController } from 'server/app.controller'
 import { AppService } from 'server/app.service'
+import { CommitmentsService } from 'server/commitments/commitments.service'
 import { SupabaseProvider } from 'server/supabase/supabase.provider'
 import { TrpcModule } from 'server/trpc/trpc.module'
 
 @Module({
   imports: [TrpcModule],
   controllers: [AlchemyProxyController, AppController],
-  providers: [AppService, SupabaseProvider],
+  providers: [AppService, CommitmentsService, SupabaseProvider],
 })
 export class AppModule {}
--- apps/server/src/commitments/entities/commitment-insert.type.ts
+++
@@ -1,3 +0,0 @@
-import type { Database } from 'server/supabase/supabase.types'
-
-export type CommitmentInsert = Database['public']['Tables']['commitments']['Insert']
