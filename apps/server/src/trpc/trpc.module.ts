12969d8df878e73efae5e6f1da80d6cedcc819a0
sripwoud <me@sripwoud.xyz>
2024-10-23 10:12:45 +0200

    fix: remove feedbacks from trpc module

--- apps/server/src/trpc/trpc.module.ts
+++ apps/server/src/trpc/trpc.module.ts
@@ -4,8 +4,8 @@ import { AuthService } from 'server/auth/auth.service'
 import { BandadaRouter } from 'server/bandada/bandada.router'
 import { BandadaService } from 'server/bandada/bandada.service'
 import { CommitmentsService } from 'server/commitments/commitments.service'
-import { FeedbacksRouter } from 'server/feedbacks/feedbacks.router'
-import { FeedbacksService } from 'server/feedbacks/feedbacks.service'
+// import { FeedbacksRouter } from 'server/feedbacks/feedbacks.router'
+// import { FeedbacksService } from 'server/feedbacks/feedbacks.service'
 import { NullifiersRouter } from 'server/nullifiers/nullifiers.router'
 import { NullifiersService } from 'server/nullifiers/nullifiers.service'
 import { RootsRouter } from 'server/roots/roots.router'
@@ -21,8 +21,8 @@ import { TrpcService } from 'server/trpc/trpc.service'
     BandadaService,
     BandadaRouter,
     CommitmentsService,
-    FeedbacksRouter,
-    FeedbacksService,
+    //  FeedbacksRouter,
+    //  FeedbacksService,
     NullifiersRouter,
     NullifiersService,
     RootsRouter,
