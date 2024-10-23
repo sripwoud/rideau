ae16068a057087d8bb432e974c41c8bd0722cda6
sripwoud <me@sripwoud.xyz>
2024-10-23 22:16:13 +0200

    fix(client): check null in enabled hook opt

--- apps/client/src/hooks/useGetGroups.ts
+++ apps/client/src/hooks/useGetGroups.ts
@@ -4,8 +4,8 @@ import { trpc } from 'client/l/trpc'
 
 export const useGetGroups = () => {
   const user = useUser()
-  return trpc.bandada.getGroupsByMemberId.useQuery(user?.commitment ? { memberId: user.commitment } : skipToken, {
-    enabled: user?.commitment !== undefined,
+  return trpc.bandada.getGroupsByMemberId.useQuery(user?.commitment === null ? skipToken : { memberId: user.commitment }, {
+    enabled: user?.commitment !== null,
     select: (groups) => groups.map(({ id, name, description }) => ({ id, name, description })),
   })
 }
