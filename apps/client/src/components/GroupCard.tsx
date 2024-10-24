18a2d7de4313c38a4b5c6a63b8cb2c9e37277c8c
sripwoud <me@sripwoud.xyz>
2024-10-24 12:43:38 +0200

    style: adjust cards padding and icons

--- apps/client/src/components/GroupCard.tsx
+++ apps/client/src/components/GroupCard.tsx
@@ -6,7 +6,7 @@ type GroupCardProps = Pick<BandadaGetGroupOutput, 'description' | 'name'>
 export const GroupCard: FC<GroupCardProps> = ({ description, name }) => {
   return (
     <div
-      className='shadow-md rounded-lg p-6'
+      className='shadow-md rounded-lg p-8'
       style={{ backgroundColor: '#fffae3', borderColor: '#5d576b', borderStyle: 'solid', borderWidth: '2px' }}
     >
       <h3 className='text-xl font-bold mb-2'>{name}</h3>
