a2679b45d286d511b3b35f3f3919c0596e5e809c
sripwoud <me@sripwoud.xyz>
2024-10-24 21:15:37 +0200

    feat: remove groups column in dashboard

--- apps/client/src/app/dashboard/page.tsx
+++ apps/client/src/app/dashboard/page.tsx
@@ -1,19 +1,14 @@
 'use client'
 import { CreateQuestionModal } from 'client/c/CreateQuestionModal'
-import { GroupCard } from 'client/c/GroupCard'
 import { Loader } from 'client/c/Loader'
 import { YNQuestionCard } from 'client/c/QuestionCard/YN'
-import { useGetGroups } from 'client/h/useGetGroups'
 import { trpc } from 'client/l/trpc'
 import { useEffect, useState } from 'react'
 import type { Question } from 'server/questions/entities'
 
 export default function Dashboard() {
   const [questions, setQuestions] = useState<Question[]>([])
-  const { data, isLoading: isLoadingQuestions } = trpc.questions.findAll.useQuery()
-
-  const { data: groups, isLoading: isLoadingGroups } = useGetGroups()
-  const isLoading = isLoadingGroups || isLoadingQuestions
+  const { data, isLoading } = trpc.questions.findAll.useQuery()
 
   useEffect(() => {
     if (data !== undefined) setQuestions(data)
@@ -30,32 +25,21 @@ export default function Dashboard() {
     },
   })
 
-  if (isLoading || groups === undefined || questions === undefined) return <Loader />
+  if (isLoading || questions === undefined) return <Loader />
 
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 gap-6 flex-grow overflow-auto'>
-      <div>
-        <div className='flex justify-center items-center justify-center mb-4'>
-          <h2 className='text-xl font-semibold'>Joined Groups</h2>
-        </div>
-        <div className='overflow-y-auto max-h-[calc(100vh-12rem)] space-y-4'>
-          {groups.map(({ id: key, ...group }) => <GroupCard key={key} {...group} />)}
-        </div>
+    <div>
+      <div className='flex items-center mb-4'>
+        <h2 className='text-xl font-semibold flex-grow text-center'>Questions</h2>
+        <CreateQuestionModal />
       </div>
-
-      <div>
-        <div className='flex items-center mb-4'>
-          <h2 className='text-xl font-semibold flex-grow text-center'>Questions</h2>
-          <CreateQuestionModal />
-        </div>
-        <div className='overflow-y-auto max-h-[calc(100vh-12rem)] space-y-4'>
-          {questions.map((question) => (
-            <YNQuestionCard
-              key={question.id}
-              {...question}
-            />
-          ))}
-        </div>
+      <div className='overflow-y-auto max-h-[calc(100vh-12rem)] space-y-4'>
+        {questions.map((question) => (
+          <YNQuestionCard
+            key={question.id}
+            {...question}
+          />
+        ))}
       </div>
     </div>
   )
