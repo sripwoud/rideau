dcf3be8d755e7f2b73b6bdf86f70399663575d11
sripwoud <me@sripwoud.xyz>
2024-10-26 20:00:30 +0200

    add roots service to app.module

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -6,12 +6,21 @@ import { AuthService } from 'server/auth/auth.service'
 import { BandadaService } from 'server/bandada/bandada.service'
 import { CommitmentsService } from 'server/commitments/commitments.service'
 import { QuestionsService } from 'server/questions/questions.service'
+import { RootsService } from 'server/roots/roots.service'
 import { SupabaseService } from 'server/supabase/supabase.service'
 import { TrpcModule } from 'server/trpc/trpc.module'
 
 @Module({
   imports: [TrpcModule],
   controllers: [AlchemyProxyController, AppController],
-  providers: [AppService, AuthService, BandadaService, CommitmentsService, QuestionsService, SupabaseService],
+  providers: [
+    AppService,
+    AuthService,
+    BandadaService,
+    CommitmentsService,
+    QuestionsService,
+    RootsService,
+    SupabaseService,
+  ],
 })
 export class AppModule {}
