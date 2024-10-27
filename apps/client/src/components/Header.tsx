8bc6ed4fd9b2f185d888940a62ea27c006abb799
sripwoud <me@sripwoud.xyz>
2024-10-27 11:01:02 +0100

    style(ui): update app icon (#71)

--- apps/client/src/components/Header.tsx
+++ apps/client/src/components/Header.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useAuth } from 'client/h/useAuth'
 import { clientConfig } from 'client/l/config'
-import Image from 'next/image'
+import { MessageCircleQuestion } from 'lucide-react'
 import Link from 'next/link'
 
 export const Header = () => {
@@ -10,8 +10,8 @@ export const Header = () => {
     <header className='p-4'>
       <ul className='flex items-center justify-between'>
         <li>
-          <Link className='flex items-center' href='/'>
-            <Image className='' src='/icon.png' alt='rideau-logo' height={50} width={50} />
+          <Link className='flex flex-row content-center' href='/'>
+            <MessageCircleQuestion size={30} />
             <span className='text-4xl'>{clientConfig.appName.toLocaleUpperCase()}</span>
           </Link>
         </li>
