ff8cbfc841a3238795dba5a4da47bd23bd920e05
sripwoud <me@sripwoud.xyz>
2024-10-27 11:17:05 +0100

    chore(server): remove `app.{controller,service}`

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common'
 import { AlchemyProxyController } from 'server/alchemy-proxy/alchemy-proxy.controller'
-import { AppController } from 'server/app.controller'
-import { AppService } from 'server/app.service'
 import { AuthService } from 'server/auth/auth.service'
 import { BandadaService } from 'server/bandada/bandada.service'
 import { CommitmentsService } from 'server/commitments/commitments.service'
@@ -12,9 +10,8 @@ import { TrpcModule } from 'server/trpc/trpc.module'
 
 @Module({
   imports: [TrpcModule],
-  controllers: [AlchemyProxyController, AppController],
+  controllers: [AlchemyProxyController],
   providers: [
-    AppService,
     AuthService,
     BandadaService,
     CommitmentsService,
