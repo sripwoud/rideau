c0dbc9f416b510f93b07acd26f825839e72cf5fc
sripwoud <me@sripwoud.xyz>
2024-10-27 21:58:12 +0100

    chore(server): remove cookie config and context

--- apps/server/src/trpc/trpc.context.ts
+++ apps/server/src/trpc/trpc.context.ts
@@ -1,14 +1,11 @@
-import { Option } from '@hazae41/option'
 import { Injectable } from '@nestjs/common'
-import type { CreateExpressContextOptions } from '@trpc/server/adapters/express'
 import { SupabaseService } from 'server/supabase/supabase.service'
 
 @Injectable()
 export class TrpcContext {
   constructor(private readonly supabase: SupabaseService) {}
 
-  create = ({ req }: CreateExpressContextOptions) => ({
-    token: Option.wrap<string>(req.cookies?.['access-token']),
+  create = () => ({
     events: this.supabase.events,
   })
 }
