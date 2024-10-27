f2e0c032f76404b2fbe1baeae1e4888bb3590a45
sripwoud <me@sripwoud.xyz>
2024-10-27 22:00:04 +0100

    chore: cleanup (#74)
    * chore: remove `contracts`
    
    * chore: delete `app.{controller,service}`
    
    * fix: update main.yml
    
    * chore: update `supabase` local config
    
    * chore(server): remove cookie config and context

--- apps/server/src/lib/config.ts
+++ apps/server/src/lib/config.ts
@@ -1,20 +1,8 @@
 import { getEnvVar, sharedConfig } from 'config'
 import { merge } from 'ts-deepmerge'
 
-export enum Cookie {
-  ACCESS = 'access-token',
-  REFRESH = 'refresh-token',
-}
-
-const ONE_HOUR_MS = 60 * 60 * 1000
-const SEVEN_DAYS_MS = 60 * 60 * 24 * 7 * 1000
-
 interface ServerConfigI {
   alchemy: { apiKey: string; urls: { api: string; rpc: string } }
-  auth: {
-    cookieMaxAge: Record<Cookie, number>
-    redirect: string
-  }
   clientUrl: string
   bandada: { apiKey: string; url: string }
   port: number
@@ -25,13 +13,6 @@ const _serverConfig: ServerConfigI = {
   alchemy: {
     apiKey: getEnvVar('ALCHEMY_API_KEY'),
     urls: { api: 'https://api.g.alchemy.com', rpc: 'https://arb-sepolia.g.alchemy.com/v2' },
-  },
-  auth: {
-    cookieMaxAge: {
-      [Cookie.ACCESS]: ONE_HOUR_MS,
-      [Cookie.REFRESH]: SEVEN_DAYS_MS,
-    },
-    redirect: 'dashboard',
   },
   bandada: {
     apiKey: getEnvVar('BANDADA_API_KEY'),
--- contracts/README.md
+++
@@ -1,5 +0,0 @@
-# Contracts
-
-| name                                   | network          | address                                                                                                                           |
-| -------------------------------------- | ---------------- | --------------------------------------------------------------------------------------------------------------------------------- |
-| [YesNoFeedback](src/YesNoFeedback.sol) | arbitrum-sepolia | [0x4308bfd6c6cd829b6d342c90b43bef46700cec8d](https://sepolia.arbiscan.io/address/0x4308bfd6c6cd829b6d342c90b43bef46700cec8d#code) |
