c92e2110691ad6c89d63b6965e674cbd71b0823b
sripwoud <me@sripwoud.xyz>
2024-10-28 14:29:38 +0100

    ci: deploy client with fly (#80)
    * ci: deploy client with fly
    
    * chore: update config
    
    * fix deploy.yml

--- config/shared.ts
+++ config/shared.ts
@@ -1,11 +1,17 @@
+import { isEnvVarDefined } from './utils'
+
 interface SharedConfigI {
   alchemy: { proxyEndpoint: string }
   appName: string
   bandada: { pseGroupId: string }
 }
 
+// @ts-expect-error 4111
+const pseGroupId = process.env.NEXT_PUBLIC_PSE_BANDADA_GROUP_ID ?? ''
+isEnvVarDefined(pseGroupId, 'NEXT_PUBLIC_PSE_BANDADA_GROUP_ID')
+
 export const sharedConfig: SharedConfigI = {
   alchemy: { proxyEndpoint: 'alchemy' },
   appName: 'yeap',
-  bandada: { pseGroupId: '48515227088583163821982124675323' },
+  bandada: { pseGroupId },
 }
