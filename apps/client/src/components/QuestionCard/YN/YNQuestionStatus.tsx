b93a51ce01d66f22d06fba1db2425acf34603a81
sripwoud <me@sripwoud.xyz>
2024-10-28 10:53:56 +0100

    refactor(ui): update `YNQuestionStatus`

--- apps/client/src/components/QuestionCard/YN/YNQuestionStatus.tsx
+++ apps/client/src/components/QuestionCard/YN/YNQuestionStatus.tsx
@@ -1,20 +1,22 @@
-import { CheckCircle, Equal, XCircle } from 'lucide-react'
+import { Equal, Hourglass, ThumbsDown, ThumbsUp } from 'lucide-react'
 import type { FC } from 'react'
 
 interface YNQuestionStatusProps {
-  yes: number
+  active: boolean
   no: number
   size: number
+  yes: number
 }
 
-export const YNQuestionStatus: FC<YNQuestionStatusProps> = ({ no, size, yes }) => {
+export const YNQuestionStatus: FC<YNQuestionStatusProps> = ({ active, no, size, yes }) => {
   const hasVotes = (yes + no) > 0
   const hasMoreYes = yes > no && hasVotes
   const hasMoreNo = no > yes && hasVotes
   const draw = yes === no && hasVotes
 
-  if (hasMoreYes) return <CheckCircle className='text-green-500' size={size} />
-  if (hasMoreNo) return <XCircle className='text-red-500' size={size} />
+  if (active) return <Hourglass size={size} />
+  if (hasMoreYes) return <ThumbsUp className='text-green-500' size={size} />
+  if (hasMoreNo) return <ThumbsDown className='text-red-500' size={size} />
   if (draw) return <Equal className='text-gray-500' size={size} />
   return
 }
--- apps/client/src/components/QuestionCard/YN/index.tsx
+++ apps/client/src/components/QuestionCard/YN/index.tsx
@@ -1,6 +1,6 @@
 import { YNQuestionStatus } from 'client/c/QuestionCard/YN/YNQuestionStatus'
 import { useSendFeedback } from 'client/h/useSendFeedback'
-import { Hourglass, ThumbsDown, ThumbsUp } from 'lucide-react'
+import { ThumbsDown, ThumbsUp } from 'lucide-react'
 import type { FC } from 'react'
 import type { Question } from 'server/questions/entities'
 
@@ -12,8 +12,6 @@ export const YNQuestionCard: FC<Question> = ({
   yes,
   no,
 }) => {
-  const hourGlassClassName = `${active === true ? '' : 'text-transparent'}`
-
   // Use the custom hook for sending feedback
   const {
     sendFeedback,
@@ -32,33 +30,30 @@ export const YNQuestionCard: FC<Question> = ({
       className='shadow-md rounded-lg p-4'
       style={{ backgroundColor: '#fffae3', borderColor: '#5d576b', borderStyle: 'solid', borderWidth: '2px' }}
     >
-      <div className='flex justify-between items-center mb-2'>
-        <Hourglass className={hourGlassClassName} size={20} />
-        <YNQuestionStatus yes={yes} no={no} size={20} />
+      <div className='flex flex-row justify-between space-x-4 mb-2'>
+        <h3 className='text-xl font-bold mb-2'>{title}</h3>
+        <YNQuestionStatus active={active} yes={yes} no={no} size={20} />
       </div>
-      <h3 className='text-xl font-bold mb-2'>{title}</h3>
-      <div className='flex justify-between items-center text-gray-600'>
-        <div>
-          <button
-            className='mr-2'
-            type='button'
-            onClick={() => {
-              sendFeedback(true)
-            }}
-            disabled={isSending}
-          >
-            {yes} <ThumbsUp className='inline-block' size={20} />
-          </button>
-          <button
-            type='button'
-            disabled={isSending}
-            onClick={() => {
-              sendFeedback(false)
-            }}
-          >
-            {no} <ThumbsDown className='inline-block' size={20} />
-          </button>
-        </div>
+      <div className='flex justify-center items-center text-gray-600'>
+        <button
+          className='mr-2'
+          type='button'
+          onClick={() => {
+            sendFeedback(true)
+          }}
+          disabled={isSending}
+        >
+          {yes} <ThumbsUp className='inline-block' size={20} />
+        </button>
+        <button
+          type='button'
+          disabled={isSending}
+          onClick={() => {
+            sendFeedback(false)
+          }}
+        >
+          {no} <ThumbsDown className='inline-block' size={20} />
+        </button>
       </div>
     </div>
   )
