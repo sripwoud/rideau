bd7bb2337bcbf2407410abe780cb5fdfe2a853cd
sripwoud <me@sripwoud.xyz>
2024-10-29 09:48:01 +0100

    refactor: remove unused groups routes

--- apps/server/src/groups/groups.router.ts
+++ apps/server/src/groups/groups.router.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common'
-import { CreateGroupDto, ExportGroupDto, FindAllGroupsDto, GroupMembersDto, MaybeJoinGroupDto } from 'server/groups/dto'
+import { ExportGroupDto, FindAllGroupsDto } from 'server/groups/dto'
 import { GroupsService } from 'server/groups/groups.service'
 import { TrpcService } from 'server/trpc/trpc.service'
 
@@ -11,12 +11,9 @@ export class GroupsRouter {
   ) {}
 
   router = this.trpc.router({
-    create: this.trpc.procedure.input(CreateGroupDto).mutation(async ({ input }) => this.groups.create(input)),
     export: this.trpc.procedure.input(ExportGroupDto).query(async ({ input }) => this.groups.export(input)),
     findAll: this.trpc.procedure.input(FindAllGroupsDto).query(
       async ({ input }) => this.groups.findAll(input),
     ),
-    maybeJoin: this.trpc.procedure.input(MaybeJoinGroupDto).mutation(async ({ input }) => this.groups.maybeJoin(input)),
-    members: this.trpc.procedure.input(GroupMembersDto).query(async ({ input }) => this.groups.members(input)),
   })
 }
