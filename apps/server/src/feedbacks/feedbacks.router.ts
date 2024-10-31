384f7eaa28f119db2691af5cbae74369ecb2e230
sripwoud <me@sripwoud.xyz>
2024-10-31 10:35:53 +0100

    feat(server): subscribe to feedback insert events

--- apps/server/src/feedbacks/feedbacks.router.ts
+++ apps/server/src/feedbacks/feedbacks.router.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common'
+import { on } from 'node:events'
 import { CreateFeedbackDto, SendFeedbackDto } from 'server/feedbacks/dto'
+import { Feedback } from 'server/feedbacks/entities'
 import { FeedbacksService } from 'server/feedbacks/feedbacks.service'
 import { TrpcService } from 'server/trpc/trpc.service'
 
@@ -13,5 +15,9 @@ export class FeedbacksRouter {
   router = this.trpc.router({
     create: this.trpc.procedure.input(CreateFeedbackDto).query(async ({ input }) => this.feedbacks.create(input)),
     send: this.trpc.procedure.input(SendFeedbackDto).mutation(async ({ input }) => this.feedbacks.send(input)),
+    onInsert: this.trpc.procedure.subscription(async function*({ ctx: { events }, signal }) {
+      for await (const [payload] of on(events, 'feedbacks.change', { signal }))
+        yield payload as { type: 'INSERT'; data: Feedback }
+    }),
   })
 }
