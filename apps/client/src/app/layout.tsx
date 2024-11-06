05a8b02d784de9c329b85697ef5a67addd84308c
sripwoud <me@sripwoud.xyz>
2024-11-06 17:52:06 +0700

    fix: use local font instead of dl from google/font

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -1,15 +1,15 @@
-import { Fira_Mono, Teko } from 'next/font/google'
 import './globals.css'
 import { cookieToInitialState } from '@account-kit/core'
 import { Layout } from 'client/c/Layout'
 import { alchemyConfig } from 'client/l/account-kit'
 import { clientConfig } from 'client/l/config'
 import { Providers } from 'client/p'
+import localFont from 'next/font/local'
 import { headers } from 'next/headers'
 import type { ReactNode } from 'react'
 
-const firaMono = Fira_Mono({ display: 'swap', subsets: ['latin'], variable: '--font-fira-mono', weight: '400' })
-const teko = Teko({ display: 'swap', subsets: ['latin'], variable: '--font-teko' })
+const firaMono = localFont({ src: '../../public/Fira_Mono/FiraMono-Regular.ttf', variable: '--font-fira-mono' })
+const teko = localFont({ src: '../../public/Teko/Teko-VariableFont_wght.ttf', variable: '--font-teko' })
 
 // @ts-ignore FIXME
 export const metadata = clientConfig.metadata
